eb413cd4f853be1794b6956013cfd5a6fd6a1b6b
KoushikBaratam <appibaratam@gmail.com>
2025-03-31 18:21:11 -0500

    Commit Message

--- Phase04/src/main/java/Login_Logout/AuthenticationService.java
+++ Phase04/src/main/java/Login_Logout/AuthenticationService.java
@@ -1,3 +1,5 @@
+package Login_Logout;
+
 import java.util.HashMap;
 import java.util.Map;
 
--- Phase04/src/main/java/Login_Logout/LoginController.java
+++ Phase04/src/main/java/Login_Logout/LoginController.java
@@ -1,6 +1,8 @@
+package Login_Logout;
+
 public class LoginController {
-    // Create an instance of the embedded authentication service
+
     private AuthenticationService authService = new AuthenticationService();
 
 
-  }
+}

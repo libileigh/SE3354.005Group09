fb22437ad82d171f35170ad58cd7321c99629fab
KoushikBaratam <appibaratam@gmail.com>
2025-04-01 21:28:25 -0500

    Commit Message

--- Phase04/src/main/java/Login_Logout/AuthenticationService.java
+++ Phase04/src/main/java/Login_Logout/AuthenticationService.java
@@ -4,26 +4,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class AuthenticationService {
+
     private Map<String, Integer> users = new HashMap<>();
+    private String loggedInUser;
     public AuthenticationService() {
 
-        users.put("koushik@gmail.com", "CrazyPapaya@123".hashCode());
-        users.put("kushal@gmail.com", "Yoshi@94".hashCode());
-        users.put("manat@gmail.com", "Manbat54".hashCode());
-        users.put("kumud@gmail.com", "Kumon12".hashCode());
-        users.put("liberty@gmail.com", "Libileigh1".hashCode());
-        users.put("david@gmail.com", "salamander@67".hashCode());
-        users.put("alice@gmail.com", "Alice@987".hashCode());
-        users.put("bob@gmail.com", "34@bob43".hashCode());
-        users.put("charlie@gmail.com", "87@6phoeniX".hashCode());
-        users.put("diana@gmail.com", "Diana@123".hashCode());
-        users.put("eve@gmail.com", "hazelnut@364".hashCode());
-        users.put("frank@gmail.com", "frank@96".hashCode());
-        users.put("george@gmail.com", "12@ruSSel".hashCode());
-        users.put("harry@gmail.com", "windsor@5985".hashCode());
-        users.put("irene@gmail.com", "Irene@123".hashCode());
+        users.put("koushik@utdallas.edu", "CrazyPapaya@123".hashCode());
+        users.put("kushal@utdallas.edu", "Yoshi@94".hashCode());
+        users.put("manat@utdallas.edu", "Manbat54".hashCode());
+        users.put("kumud@utdallas.edu", "Kumon12".hashCode());
+        users.put("liberty@utdallas.edu", "Libileigh1".hashCode());
+        users.put("david@utdallas.edu", "salamander@67".hashCode());
+        users.put("alice@utdallas.edu", "Alice@987".hashCode());
+        users.put("bob@utdallas.edu", "34@bob43".hashCode());
+        users.put("charlie@utdallas.edu", "87@6phoeniX".hashCode());
+        users.put("diana@utdallas.edu", "Diana@123".hashCode());
+        users.put("eve@utdallas.edu", "hazelnut@364".hashCode());
+        users.put("frank@utdallas.edu", "frank@96".hashCode());
+        users.put("george@utdallas.edu", "12@ruSSel".hashCode());
+        users.put("harry@utdallas.edu", "windsor@5985".hashCode());
+        users.put("irene@utdallas.edu", "Irene@123".hashCode());
     }
-
-
 
 }

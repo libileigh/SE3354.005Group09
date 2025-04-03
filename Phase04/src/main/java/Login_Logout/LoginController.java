4922262ea9a553d3d4ebbe5448a00739c2ae5cb1
KoushikBaratam <appibaratam@gmail.com>
2025-04-02 21:45:57 -0500

    commit message

--- Phase04/src/main/java/Login_Logout/LoginController.java
+++ Phase04/src/main/java/Login_Logout/LoginController.java
@@ -5,14 +5,18 @@ public class LoginController {
     public AuthenticationService authService = new AuthenticationService();
     public String login(String email, String password) {
         if (authService.login(email, password)) {
+
             return "Login successful. Welcome " + authService.getLoggedInUser();
         }
+
         return "Invalid credentials. Please try again.";
     }
 
     public String logout() {
         if (authService.isLoggedIn()) {
+
             authService.logout();
+
             return "Logged out successfully.";
         }
         return "No user is currently loggedin.";
